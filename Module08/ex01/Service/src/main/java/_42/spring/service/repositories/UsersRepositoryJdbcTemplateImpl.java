160b215cab0196fce58c0661bae8f33554ea3147
oualidobbad <oualidobbad@gmail.com>
2026-09-05 18:28:51 +0100

    i implementing JdbcTemplate

--- Module08/ex01/Service/src/main/java/_42/spring/service/repositories/UsersRepositoryJdbcTemplateImpl.java
+++ Module08/ex01/Service/src/main/java/_42/spring/service/repositories/UsersRepositoryJdbcTemplateImpl.java
@@ -1,15 +1,34 @@
 package _42.spring.service.repositories;
 
 import _42.spring.service.models.User;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import java.util.List;
 import java.util.Optional;
+import _42.spring.service.models.*;
 
 public class UsersRepositoryJdbcTemplateImpl implements UsersRepository{
 
+    private final JdbcTemplate jdbcTemplate;
+
+//    private RowMapper rowMapper = (rs, numRows) -> (
+//            new User(rs.getLong("id"), rs.getString("email"))
+//    );
+
+    private RowMapper rowMapper = (rs, rowNum) -> (
+            User user = new User();
+            user.setId(rs.getLong("id"));
+            user.setEmail(rs.getString("email"));
+            );
+    public UsersRepositoryJdbcTemplateImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public Optional<User> findByEmail(String email) {
-        return Optional.empty();
+
+        return jdbcTemplate.;
     }
 
     @Override
