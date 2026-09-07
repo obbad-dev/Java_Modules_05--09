69ded7f8b9e3f24a3dad89bc3b288238aab85814
oobbad-dev <oualidobbad@gmail.com>
2026-09-07 17:11:20 +0000

    done

--- Module08/ex01/Service/src/main/java/_42/spring/service/application/Main.java
+++ Module08/ex01/Service/src/main/java/_42/spring/service/application/Main.java
@@ -2,64 +2,56 @@ package _42.spring.service.application;
 
 import _42.spring.service.models.User;
 import _42.spring.service.repositories.UsersRepository;
-import _42.spring.service.repositories.UsersRepositoryJdbcImpl;
-import _42.spring.service.repositories.UsersRepositoryJdbcTemplateImpl;
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
 
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
-    static HikariConfig setupConfig() {
-        HikariConfig hc = new HikariConfig();
-        hc.setJdbcUrl("jdbc:postgresql://localhost:5432/testdb");
-        hc.setUsername("postgres");
-        hc.setPassword("postgres");
-        hc.setDriverClassName("org.postgresql.Driver");
-        return hc;
-    }
     public static void main(String [] args) {
-        try (HikariDataSource dataSource = new HikariDataSource(setupConfig())) {
-            UsersRepository usersRepository;
-            
+
+        try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml")) {
+
+            UsersRepository usersRepository;            
             System.out.println("=================== JDBC Implementation ==================");
-            
-            usersRepository = new UsersRepositoryJdbcImpl(dataSource);
-            User user = new User(null, "hassan@gmail.com");
+            usersRepository = context.getBean("userRepositoryJdbc", UsersRepository.class);
+            User user = context.getBean("userTest", User.class);
+
             usersRepository.save(user);
-            Optional<User> us = usersRepository.findByEmail("hassan@gmail.com");
+            Optional<User> us = usersRepository.findByEmail("JDBC@Template");
             us.ifPresentOrElse(
                     (user1 -> System.out.println("user added success: " + user1)),
                     () -> System.out.println("user not found")
             );
 
-            user.setEmail("hwisini@42.fr.com");
+            user.setEmail("JDBC@TemplateUpdated");
             usersRepository.update(user);
             System.out.println("user updated succefully: " + usersRepository.findById(user.getId()));
             usersRepository.delete(user.getId());
             System.out.println("user deleted succefully: " + user);
             System.out.println("ALL USERS");
             List<User> users = usersRepository.findAll();
-            for (User user2 : users)
+            for (User user1 : users)
             {
-                System.out.println(user2);
+                System.out.println(user1);
             }
 
             System.out.println("=================== JDBC Template Implementation ==================");
-            usersRepository = new UsersRepositoryJdbcTemplateImpl(dataSource);
-            User user1 = new User(null, "JDBC@Template");
-            usersRepository.save(user1);
+            usersRepository = context.getBean("userRepositoryTemplateJdbc", UsersRepository.class);
+            user = context.getBean("userTest", User.class);
+            System.out.println(user);
+            usersRepository.save(user);
             Optional<User> us1 = usersRepository.findByEmail("JDBC@Template");
             us1.ifPresentOrElse(
                     (user2 -> System.out.println("user added success: " + user2)),
                     () -> System.out.println("user not found")
             );
-            user1.setEmail("JDBC@TemplateUpdated");
-            usersRepository.update(user1);
-            System.out.println("user updated succefully: " + usersRepository.findById(user1.getId()));
-            usersRepository.delete(user1.getId());
-            System.out.println("user deleted succefully: " + user1);
+            user.setEmail("JDBC@TemplateUpdated");
+            usersRepository.update(user);
+            System.out.println("user updated succefully: " + usersRepository.findById(user.getId()));
+            usersRepository.delete(user.getId());
+            System.out.println("user deleted succefully: " + user);
             System.out.println("ALL USERS");
             List<User> users1 = usersRepository.findAll();
             for (User user2 : users1)
@@ -71,7 +63,5 @@ public class Main {
         {
            e.printStackTrace();
         }
-    }
-
-    
+    }    
 }
